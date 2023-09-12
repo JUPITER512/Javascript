5ae3a6758a4bafb9e7724442d56d7a3a2bdb5f7f
JUPITER512 <103397948+JUPITER512@users.noreply.github.com>
2023-09-12 07:46:17 +0000

    variables_datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js as a newer version
+// alert("Hello") ==> this won't run in node js but it would run in browser
+//code should be readable
+console.log(3+3);
+
+// --------------------------------------Premitive datatype---------------------------------------------------------------
+//number ==> 2 to power 53 2**53
+//big init==>
+//string '' ""
+//boolean true/false
+//null ==> standalone value its a datatype (representation value of empty variable)
+//undefined ==> when we don;t defined value
+//symbol ==> for uniqueness we use symbol
+//object
+// --------------------------------------Premitive datatype---------------------------------------------------------------
+
+let name='Ali Murtaza';
+let age=23;
+let isloggedIn=false;
+
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId=143214;
+let accountEmail="syedalimurtaza36@gmail.com";
+var accountpassword="123456i9";
+accountCity='Lahore';//===>not prefer
+let accountState;
+
+// accountId=2;
+accountEmail='syedalimurtaza52@gmail.com';
+accountpassword='121345';
+accountCity='Islamabad';
+console.log(accountId)
+/*
+    Const can't be changed
+    Prefer not to use VAR 
+    Because of issue in block scope 
+    and Function Scope
+ */
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+// accountEmail='alimurtaza@gmail'
