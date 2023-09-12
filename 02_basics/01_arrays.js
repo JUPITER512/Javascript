50e28beceabe1eb6a2b38e9f993422633b9351fb
JUPITER512 <103397948+JUPITER512@users.noreply.github.com>
2023-09-12 13:10:15 +0000

    array and array methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// const myArr=[0,1,2,3,4,5,true,'Ali']
+// console.log(myArr[1]);
+// const myHeros=['Ironman','Superman']
+
+
+// const myArr2=new Array(1,2,3,4)
+
+// //array methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+
+// const newarrr=myArr.join()
+// console.log(typeof (newarrr));
+
+
+
+
+let myArr=[1,2,3,4,5,6]
+
+//slice , splice
+console.log("A ",myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr);
+const myn2=myArr.splice(1,3)
+console.log('After splice method'+myn2)
+console.log('After splice method'+myArr)
