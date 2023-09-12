f98070b29a4a2046164a4d3b5806effcbaa704f6
JUPITER512 <103397948+JUPITER512@users.noreply.github.com>
2023-09-12 11:06:02 +0000

    numbersAndMaths operations

---
+++ 01_basics/numbersAndMaths.js
@@ -0,0 +1,49 @@
+ const score=300
+ 
+ const balance=new Number(400)
+ console.log(balance)
+
+
+ console.log(balance.toString().length)
+ console.log(balance.toFixed(2))
+
+
+ const otherNumber=23.21341451
+ console.log(otherNumber)
+ console.log(otherNumber.toPrecision(3))
+ console.log(otherNumber.toFixed(2))
+
+
+
+ const hundreds=1000000
+ console.log(hundreds.toLocaleString('en-IN'))
+
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+console.log(' -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-')
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.65))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.sqrt(25))
+console.log(Math.pow(2,5))
+console.log(Math.min(4,10,20,1,3,2))
+console.log(Math.max(1,2,3,4,5,0,-1))
+console.log(' -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-')
+console.log(Math.random()*10)// between 0 and 1
+console.log((Math.random()*10)+1)// between 0 and 1 // +1 to avoid the case inwhich 0.02 etc comes
+
+
+console.log(' -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-')
+const min=10
+const max=20
+console.log((Math.random()*(max-min+1))+min)
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+ 
